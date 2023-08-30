f376b24aee7d22a01d0a4ccda32440be039018dd
ALEXDRUGS <alex.drugs1979@gmail.com>
2023-08-30 20:28:10 +0300

    Поиск и вывод целого числа в строке

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(stringToInt("-+283472"));
     }
 
     /**
@@ -24,5 +27,43 @@ public class Main {
             }
         }
         return nums;
+    }
+
+    private static int stringToInt(String s) {
+        Pattern pattern = Pattern.compile("[:punct]|[^:alnum]");
+        Matcher matcher = pattern.matcher(s);
+        char[] charArray = s.toCharArray();
+        char[] buffer = new char[charArray.length];
+        int count = 0;
+        int iterate = 0;
+        for (char symbol : charArray) {
+            if (Character.isDigit(symbol) || symbol == '-') {
+                buffer[count] = symbol;
+                count++;
+            }
+            if (symbol == ' ' && iterate == 0) {
+                continue;
+            }
+            if (symbol == ' ' && count == 0) {
+                return 0;
+            }
+            if (matcher.find() && iterate != 0) {
+                String string = new String(buffer);
+                if (string.startsWith("-")) {
+                    return -Integer.parseInt(string.substring(1, count));
+                } else return Integer.parseInt(string.substring(0, count));
+            }
+            if (matcher.find() && iterate == 0){
+                return 0;
+            }
+            iterate++;
+        }
+        if (count > 9) {
+            return Integer.MIN_VALUE;
+        }
+        String string = new String(buffer);
+        if (string.startsWith("-")) {
+            return -Integer.parseInt(string.substring(1, count));
+        } else return Integer.parseInt(string.substring(0, count));
     }
 }
